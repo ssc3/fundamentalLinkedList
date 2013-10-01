3e27da6a1c983bc6f49dbaf2378d900f409b8f41
ssc3 <ssc3@ssc3.(none)>
2013-09-30 23:55:30 -0700

    second commit

--- linked_list.cc
+++ linked_list.cc
@@ -49,7 +49,26 @@ class List {
             return true;
         }
 
+        Node* GetTail()
+        {
+            if (tail == NULL)
+                return NULL;
+            else
+                return tail;
+        }
 
+        bool CheckTail()
+        {
+            if (tail == NULL)
+            {
+                printf ("No Tail\n");
+                return false;
+            }
+            return true;
+        }
+
+        // Inserts at the tail of list
+        // by traversing the whole list
         void Insert(Node* temp_head, int key)
         {
             if (temp_head == NULL)
@@ -70,6 +89,44 @@ class List {
             Insert(temp_head->next, key);
         }
 
+        // Insert directly at the tail of the list
+        // using Tail pointer
+        void FastInsert(int key)
+        {
+            if (!CheckTail())
+                return;
+
+            Node* new_node = new Node(key);
+            tail->next = new_node;
+        }
+
+
+
+        // different from Insert
+        // Insert inserts at tail
+        // Push inserts at head
+        void Push(int key)
+        {
+
+            if (!CheckHead())
+                return;
+
+            Node* new_node = new Node(key);
+            new_node->next = head;
+            head = new_node;
+        }
+
+        // Remove the head
+        void Pop()
+        {
+            if (!CheckHead())
+                return;
+
+            Node* temp_node = head;
+            head = head->next;
+            delete temp_node;
+        }
+
         void ChangeHead(Node* node, int key)
         {
             if (!CheckHead())
@@ -84,6 +141,25 @@ class List {
             ChangeHead(node->next, key);
         }
 
+        Node* GetNth(int node_num)
+        {
+            if (!CheckHead())
+                return NULL;
+
+            if (node_num > num_nodes - 1)
+            {
+                printf("Can't find %dth node. Only %d nodes in list\n", node_num, num_nodes);
+                return NULL;
+            }
+
+            Node* temp_node = head;
+            for (int i = 0; i < node_num-1; i++)
+            { 
+                temp_node = temp_node->next;
+
+            }
+            return temp_node;
+        }
             
 
         void PrintFullList()
@@ -105,9 +181,54 @@ class List {
                 else
                 {
                     printf("key = %d\n", temp_node->key);
+                    printf("*****\n");
                     temp_node = NULL;
                 }
 
+            }
+        }
+
+        void PrintLength()
+        {
+            if (!CheckHead())
+                return;
+
+            int count = 0;
+            Node* temp_node = head;
+
+            while (temp_node != NULL)
+            {
+                count++;
+                if (temp_node->next != NULL)
+                    temp_node = temp_node->next;
+
+                else 
+                    break;
+            }
+            num_nodes = count;
+            printf("Length of list = %d\n", count);
+        }
+
+        void DeleteList()
+        {
+            if(!CheckHead())
+                return;
+
+            Node* temp_node = head;
+
+            while (temp_node != NULL)
+            {
+                if (temp_node->next == NULL)
+                {
+                    delete temp_node;
+                    head = NULL;
+                    tail = NULL;
+                    return;
+                }
+
+                Node* temp2 = temp_node->next;
+                delete temp_node;
+                temp_node = temp2;
             }
         }
 
@@ -115,6 +236,7 @@ class List {
     private:
         Node* head;
         Node* tail;
+        int num_nodes;
 };
 
 
@@ -140,6 +262,22 @@ int main(int argc, char* argv[])
     my_list->ChangeHead(my_list->GetHead(), 2);
     my_list->PrintFullList();    
 
+    my_list->Push(7);
+    my_list->PrintFullList();    
+
+    my_list->FastInsert(9);
+    my_list->PrintFullList();    
+    my_list->PrintLength();
+
+    Node* local_node = my_list->GetNth(3);
+    printf("Got %dth node. Key = %d\n", 3, local_node->key);
+    printf("*****\n");
+
+    my_list->Pop();
+    my_list->PrintFullList();    
+
+    my_list->DeleteList();
+    my_list->PrintFullList();    
 
 
 
